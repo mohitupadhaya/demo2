fdcc7b2ac7cdf09cdcf675ac902918f5b5d52720
Mohit Upadhaya <mohitu@chetu.com>
2022-04-22 18:55:39 +0530

    first commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Chetu_Team',
+        __DIR__
+    );
